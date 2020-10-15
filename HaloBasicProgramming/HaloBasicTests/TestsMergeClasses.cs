72dd44082c426b1de3a9d6ae206b32a63d212af2
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2020-10-16 00:05:54 +0300

    Halo - Merge Classes

---
+++ HaloBasicProgramming/HaloBasicTests/TestsMergeClasses.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HaloBasicTests
+{
+    [TestClass]
+    public class TestsMergeClasses
+    {
+        [TestMethod]
+        public void TestMergeClasses_Case0()
+        {
+            var result = HaloBasicProgramming.TaskMergeClasses.merge(
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5 });
+
+            Assert.IsTrue(CompareArrays(result, new int[] { 1, 2, 3, 4, 5 }), "");
+        }
+
+
+        private bool CompareArrays(int[] actual, int[] expected)
+        {
+            if (actual.Length != expected.Length)
+                return false;
+
+            for(int i=0; i < actual.Length; i++)
+            {
+                if (actual[i] != expected[i])
+                    return false;
+            }
+
+            return true;
+        }
+
+
+
+//        Sample Input 0
+
+
+
+//Sample Output 0
+
+
+//Sample Input 1
+
+//1,2,4
+//3,5,6
+//Sample Output 1
+
+//1,2,3,4,5,6
+//Sample Input 2
+
+//1,3
+//1,2,3,4
+//Sample Output 2
+
+//1,1,2,3,3,4
+    }
+}
---
+++ HaloBasicProgramming/ProgramMergeClasses.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+
+namespace HaloBasicProgramming
+{
+    public class TaskMergeClasses
+    {
+
+        public static int[] merge(int[] a, int[] b)
+        {
+            // Write Your Code Here
+
+            List<int> list = new List<int>();
+            int ia = 0;
+            int ib = 0;
+            while (ia < a.Length && ib < b.Length)
+            {
+                if (a[ia] < b[ib])
+                {
+                    list.Add(a[ia]);
+                    ia++;
+                }
+                else
+                {
+                    list.Add(b[ib]);
+                    ib++;
+                }
+            }
+            while (ia < a.Length)
+            {
+                list.Add(a[ia]);
+                ia++;
+            }
+            while (ib < b.Length)
+            {
+                list.Add(b[ib]);
+                ib++;
+            }
+
+            return list.ToArray();
+
+        }
+
+        static void MainMergeClasses(string[] args)
+        {
+            TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int[] a = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+            int[] b = Array.ConvertAll(Console.ReadLine().Split(','), int.Parse);
+
+            int[] result = merge(a, b);
+
+            tw.WriteLine(string.Join(",", result));
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+}
