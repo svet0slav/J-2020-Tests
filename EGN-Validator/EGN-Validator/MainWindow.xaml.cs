762f6a8592e7580e4cdddd10e47201c20b40540f
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2020-10-27 23:12:23 +0200

    EGN-Validator application

---
+++ EGN-Validator/EGN-Validator/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using EGNValidator.Service.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EGN_Validator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var validator = new ENGValidator();
+            if (validator.Validate(txtEGNData.Text))
+            {
+                txtErrors.Text = "Коректно";
+            }
+            else
+            {
+                txtErrors.Text = "Некоректно.\r\n";
+                foreach(var err in validator.Validation.Errors)
+                {
+                    txtErrors.Text += err.Message + "\r\n";
+                }
+            }
+
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            txtEGNData.Focus();
+        }
+    }
+}
