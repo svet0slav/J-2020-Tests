9de5a4d3049c895215791498a40642d57220545c
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2020-11-05 20:22:18 +0200

    SmallestPositiveInteger - initial solution for 14-20 minutes.

---
+++ CodilityTests/SmallestPositiveInteger/SmallestPositiveIntegerClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityTests.SmallestPositiveInteger
+{
+    public class SmallestPositiveIntegerClass
+    {
+        // you can also use imports, for example:
+        // import java.util.*;
+        // you can write to stdout for debugging purposes, e.g.
+        // System.out.println("this is a debug message");
+        // write your code in Java SE 8
+
+        public int Solution1(int[] A)
+        {
+                
+            //int min = A[0];
+            int seq = 1;
+            int N = A.Length;
+            //int minIndex = 0;
+            Boolean used = true;
+            int i;
+            while (used)
+            {
+                used = false;
+                for (i = 0; i < N; i++)
+                {
+                    if (seq == A[i])
+                    {
+                        seq++;
+                        //minIndex = i + 1;
+                        used = true;
+                    }
+                }
+            }
+            return seq;
+        }
+    }
+}
---
+++ CodilityTests/UnitTests/SmallesPositiveInteger/SmallestPositiveInteger.cs
@@ -0,0 +1,80 @@
+﻿using CodilityTests.SmallestPositiveInteger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SmallestPositiveInteger
+    {
+        [TestMethod]
+        public void CoverBuildingsTests_Test1()
+        {
+            int[] data1 = new int[] { 1, 3, 6, 4, 1, 2 };
+            var test = new SmallestPositiveIntegerClass();
+            var result = test.Solution1(data1);
+            Assert.AreEqual(5, result, "Example 1 [1, 3, 6, 4, 1, 2] failed with solution 1");
+
+            //result = test.Solution2(data1);
+            //Assert.AreEqual(5, result, "Example 1 [1, 3, 6, 4, 1, 2] failed with solution 2");
+        }
+
+        [TestMethod]
+        public void CoverBuildingsTests_Test2()
+        {
+            int[] data1 = new int[] { 1, 2, 3 };
+            var test = new SmallestPositiveIntegerClass();
+            var result = test.Solution1(data1);
+            Assert.AreEqual(4, result, "Example 1 [1, 2, 3] failed with solution 1");
+
+            //result = test.Solution2(data1);
+            //Assert.AreEqual(4, result, "Example 1 [1, 2, 3] failed with solution 2");
+        }
+
+        [TestMethod]
+        public void CoverBuildingsTests_Test3()
+        {
+            int[] data1 = new int[] { -1, -3 };
+            var test = new SmallestPositiveIntegerClass();
+            var result = test.Solution1(data1);
+            Assert.AreEqual(1, result, "Example 1 [−1, −3] failed with solution 1");
+
+            //result = test.Solution2(data1);
+            //Assert.AreEqual(5, result, "Example 1 [-1, -3] failed with solution 2");
+        }
+
+        [TestMethod]
+        public void CoverBuildingsTests_Test4()
+        {
+            int[] data1 = new int[100000];
+            var rnd = new Random();
+            for (int i = 0; i < 100000; i++)
+            {
+                data1[i] = rnd.Next(100000);
+            }
+
+            var start = DateTime.Now;
+            var test = new SmallestPositiveIntegerClass();
+            var result = test.Solution1(data1);
+            var lasts = DateTime.Now.Subtract(start).TotalMilliseconds;
+            Assert.IsTrue(lasts < 1000, "Example 100,000 - more than 1 seconds. Failed with solution 1.");
+        }
+
+        [TestMethod]
+        public void CoverBuildingsTests_Test5()
+        {
+            int[] data1 = new int[1000000];
+            var rnd = new Random();
+            for (int i = 0; i < 1000000; i++)
+            {
+                data1[i] = rnd.Next(100000);
+            }
+
+            var start = DateTime.Now;
+            var test = new SmallestPositiveIntegerClass();
+            var result = test.Solution1(data1);
+            var lasts = DateTime.Now.Subtract(start).TotalMilliseconds;
+            Assert.IsTrue(lasts < 6000, "Example 1,000,000 - more than 6 seconds. Failed with solution 1.");
+        }
+    }
+}
