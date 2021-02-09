5e3e2e311296ee66c55151fba39c58f23d889d3e
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2021-02-09 14:54:07 +0200

    Basic Programming - Away Out task (stack of brackets)

---
+++ HaloBasicProgramming/HaloBasicTests/TestAwayOut.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HaloBasicTests
+{
+    [TestClass]
+    public class TestAwayOut
+    {
+        [TestMethod]
+        public void TestAwayOut_TestSample1_Positive()
+        {
+            string data = "[()]{}{()()}";
+            var actual = TaskAwayOut.isValid(data);
+
+            Assert.AreEqual(true, actual, $"Invalid for \"{data}\"");
+        }
+
+        [TestMethod]
+        public void TestAwayOut_TestSample2_Negative()
+        {
+            string data = "[(])";
+            var actual = TaskAwayOut.isValid(data);
+
+            Assert.AreEqual(true, actual, $"Invalid for \"{data}\"");
+        }
+
+        [TestMethod]
+        public void TestAwayOut_TestSample3_Negative()
+        {
+            string data = "[({}][()])";
+            var actual = TaskAwayOut.isValid(data);
+
+            Assert.AreEqual(true, actual, $"Invalid for \"{data}\"");
+        }
+    }
+}
---
+++ HaloBasicProgramming/ProgramAwayOut.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace HaloBasicTests
+{
+    public class TaskAwayOut
+    {
+        public static bool isValid(string s)
+        {
+            // Write Your Code Here
+            if (string.IsNullOrEmpty(s))
+                return false;
+
+            Dictionary<char, char> matches = new Dictionary<char, char>(9)
+            { { '(', ')'}, {'[',']'}, {'{','}'}};
+
+            Stack<char> stack = new Stack<char>();
+            int l = s.Length;
+            for (int i = 0; i < l; i++)
+            {
+                var c = s[i];
+                if (matches.Keys.Contains(c))
+                {
+                    stack.Push(c);
+                }
+                else if (matches.Values.Contains(c))
+                {
+                    var p = stack.Peek();
+                    if (matches[p] == c)
+                    {
+                        if (stack.Count > 0)
+                        {
+                            stack.Pop();
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                    else
+                    {
+                        return false; //do not match
+                    }
+                }
+            }
+
+            return stack.Count == 0; //nothing left.
+
+        }
+
+        static void MainAwayOut(string[] args)
+        {
+            TextWriter tw = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string s = Console.ReadLine();
+
+            bool result = isValid(s);
+
+            tw.WriteLine(result.ToString().ToLower());
+
+            tw.Flush();
+            tw.Close();
+        }
+    }
+}
