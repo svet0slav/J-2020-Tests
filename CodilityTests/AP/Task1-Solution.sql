fc79d79bc9d787dbfceb887c466f938111ed6bb5
Svetoslav Ivanov <svetoslav.ivanov@company.com>
2021-02-09 04:48:28 +0200

    CodilityTest AP - task 1 and all in project. Initial results.

---
+++ CodilityTests/AP/Task1-Solution.sql
@@ -0,0 +1,9 @@
+﻿select URGRP.UserName,
+    URGRP.NoOfCreatedRoles,
+    (select Count(UR2.CreatedBy) FROM UserRole as UR2 WHERE UR2.IsEnabled = true and URGRP.UserName = UPPER(TRIM(BOTH ' ' from UR2.CreatedBy))) as NoOfCreatedAndEnabledRoles,
+    (select Count(UR3.Updated) FROM UserRole as UR3 WHERE URGRP.UserName = UPPER(TRIM(BOTH ' ' from UR3.UpdatedBy)) )  as    NoOfUpdatedRoles
+    FROM 
+        (SELECT UPPER(TRIM(BOTH ' ' from UR.CreatedBy)) as UserName, Count(UR.CreatedBy) as NoOfCreatedRoles
+            from UserRole as UR
+            where LENGTH(UR.CreatedBy) > 0
+            group by UserName) as URGRP
